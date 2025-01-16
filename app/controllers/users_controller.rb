febe0529996e102a10005723aa9c8cae48bacd91
Alonzo Rice <96135977+ricealonzo@users.noreply.github.com>
2025-01-16 18:39:34 +0000

    show_table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,9 @@ class UsersController < ApplicationController
    @list_of_users = User.all.order({:username => :asc})
     render({:template => "user_templates/index"}) 
   end
+   def show
+    url_username = params.fetch("url_username")
+    @one_user = User.where({:username => url_username}).at(0)
+      render({:template=> "user_templates/show"})
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
 get("/users", {:controller =>"users" , :action=>"index"})
+get("/users/:url_username", {:controller =>"users", :action =>"show"})
 
 end
