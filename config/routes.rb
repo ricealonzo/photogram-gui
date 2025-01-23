01fdb7d05bfba7aa16b6368bdeca4c1de86ad745
Alonzo Rice <96135977+ricealonzo@users.noreply.github.com>
2025-01-23 20:03:33 +0000

    delete works finish update add comment add user

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 get("/users", {:controller =>"users" , :action=>"index"})
 get("/users/:url_username", {:controller =>"users", :action =>"show"})
-
 get("/photos", {:controller =>"photos", :action => "index"})
 get("/photos/:path", {:controller =>"photos", :action => "show"})
-post("/delete_photo/:path" , {:controller =>"photos", :action => "destroy"})
+
+get("/delete_photo/:path" , {:controller =>"photos", :action => "destroy"})
+
 post("/insert_photo" , {:controller => "photos", :action => "create"})
+
 post("/update_photo" , {:controller => "photos", :action => "update"})
 
 
